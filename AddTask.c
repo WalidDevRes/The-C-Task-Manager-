215d8f919584f9371d6732dc1101fc2cbb24ec95
Walid <watermanfr91@gmail.com>
2024-04-11 01:12:32 +0200

    Premier ajout connection --> mysql (Sa ne fonctionen pas )

--- AddTask.c
+++ AddTask.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "MySQL.c"
 
 void AddTask() {
     char Tache[100];
@@ -8,7 +9,7 @@ void AddTask() {
     printf("Veuillez ajouter votre tache a faire: ");
     scanf(" %[^\n]", Tache); 
     printf("%s\n", Tache);
-
+    MySQL();
     return;
 }
    
